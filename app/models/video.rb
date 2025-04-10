5d494103958382fae4d25ace8239bf2ad4320c55
usamatahir <usamatahir159@gmail.com>
2025-04-10 23:50:24 +0500

    add default status for video

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,8 +3,10 @@ class Video < ApplicationRecord
 
   validates :title, presence: true
   validates :video_file, presence: true
-  validates :video_file, content_type: ['video/mp4', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv']
+  validates :video_file, content_type: [ "video/mp4", "video/quicktime", "video/x-msvideo", "video/x-ms-wmv" ]
   validates :video_file, size: { less_than: 100.megabytes }
+
+  after_initialize :set_default_status, if: :new_record?
 
   enum :status, {
     pending: 'pending',
@@ -14,7 +16,13 @@ class Video < ApplicationRecord
   }
 
   enum :platform_type, {
-    youtube: 'youtube',
-    tiktok: 'tiktok'
+    youtube: "youtube",
+    tiktok: "tiktok"
   }
+
+  private
+
+  def set_default_status
+    self.status ||= :pending
+  end
 end
